8ea148a58bb633dc7a261384bb25a8a23cc84a9f
Lars Eckart <lars.eckart@hey.com>
2021-02-22 08:54:59 +0200

    r extract methods to unify

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -19,11 +19,23 @@ public class Game {
 
     public Game() {
         for (int i = 0; i < 50; i++) {
-            popQuestions.addLast("Pop Question " + i);
-            scienceQuestions.addLast(("Science Question " + i));
-            sportsQuestions.addLast(("Sports Question " + i));
+            popQuestions.addLast(createPopQuestion(i));
+            scienceQuestions.addLast(createScienceQuestion(i));
+            sportsQuestions.addLast(createSportsQuestion(i));
             rockQuestions.addLast(createRockQuestion(i));
         }
+    }
+
+    private String createPopQuestion(int i) {
+        return "Pop Question " + i;
+    }
+
+    private String createScienceQuestion(int i) {
+        return "Science Question " + i;
+    }
+
+    private String createSportsQuestion(int i) {
+        return "Sports Question " + i;
     }
 
     public String createRockQuestion(int index) {
